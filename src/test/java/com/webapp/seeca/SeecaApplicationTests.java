281d5b1702f1c5b429672f2d4bf679dccd5c3f9b
Minseok-sms <tjalstjr111@gmail.com>
2022-02-06 16:40:26 +0900

    start proj

---
+++ src/test/java/com/webapp/seeca/SeecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.seeca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
